1b6aa48aa2916bc4cffc58403c6d99362f57c85f
Johan Zicola <johan.zicola@gmail.com>
2018-09-04 09:44:53 +0200

    Add code so that the script can be called from the script directory in any other directory and still get linked the dependent scripts. Tested.

--- generate_phenotype_gemma.sh
+++ generate_phenotype_gemma.sh
@@ -3,6 +3,10 @@
 # Third argument => region
 # Fourth argument => accessions_order file
 
+# The script can be run from another directory as long as dependent scripts are located in same directory as caller script (i.e. this script)
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
 
 generate_phenotype_gemma(){
 	rda_object=$1
@@ -19,11 +23,11 @@ generate_phenotype_gemma(){
 	
 	# Make readable dataframe for 1 methylation context
 	echo "Rscript subset_df_methylation.R $context $rda_object ${name_df}.txt"
-	Rscript subset_df_methylation.R $context $rda_object ${name_df}.txt
+	Rscript ${ABSDIR}/subset_df_methylation.R $context $rda_object ${name_df}.txt
 	
 	# Generate a file with only methylation data, in the order specified in the VCF file used for GWAS
 	echo "Rscript create_gemma_phenotype.R $accessions_order df_accessions.txt ${name_df}.txt $gemma_file"
-	Rscript create_gemma_phenotype.R $accessions_order df_accessions.txt ${name_df}.txt $gemma_file
+	Rscript ${ABSDIR}/create_gemma_phenotype.R $accessions_order df_accessions.txt ${name_df}.txt $gemma_file
 	
 	# Copy file in output directory
 	echo "cp  $gemma_file /srv/biodata/dep_coupland/grp_hancock/johan/GWAS/dna_methylation/"
@@ -31,5 +35,5 @@ generate_phenotype_gemma(){
 }
 
 # Example:
-# generate_phenotype_gemma df_mean_intergenic_regions.rda CHH intergenic_regions 
+# generate_phenotype_gemma df_mean_intergenic_regions.rda CHH intergenic_regions order_accessions.txt
 generate_phenotype_gemma $1 $2 $3 $4
