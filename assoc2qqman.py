f84ebdf33d67cb7f29f60bb842a89010c36b59a3
Johan Zicola <johan.zicola@gmail.com>
2019-07-12 17:14:22 +0200

    Fix bug of assoc2qqman.py when chromosome name is only a digit. Creates a checkpoint to also remove chromosomes such as ChrM, ... Tested and works

--- assoc2qqman.py
+++ assoc2qqman.py
@@ -12,10 +12,24 @@ import re
     Read the output of gemma 'prefix.assoc.txt' derived 
     from the association analysis command and performs 
     readjustment of the file to be compatible for Read
-    R analysis with the package 'qqman'.
+    R analysis with the package 'qqman'. Note that chromosomes
+    should either be named as digits or digits with prefix "Chr"
+    as e.g Chr3. All other nomenclatures will be removed from the
+    output file.
     Compatible Python27 and Python37
 '''
 
+
+
+# Function to test whether the string following "Chr" 
+# in the chromosome name is a digit or not. This allow
+# to discard lines with ChrM or ChrC for instance
+def is_digit(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
 
 def main():
 
@@ -29,21 +43,32 @@ def main():
     else:
         sys.exit("Error: input file does not exist")
 
-
+    # Get through the file and parse the fiels to reorganize them as expected
+    # by manahattan R package
     for line in lines_file:
-        if line[:3] == "chr":  #First line should be header
+        if line[:3] == "chr":  #First line should be header (chr per default in gemma output)
             header = "SNP\tCHR\tBP\tP\tzscore"
             print(header)
         else:
             line = line.strip().split("\t")  # Get rid of EOL and Create a list based on \t separation
             SNP = line[1].replace(":","_")  # Second column but replace column by underscore
-            chr_name = line[1].split(":")
-            #Keep only lines matching chromosomes (exclude Mt, Pt, ...)
-            if chr_name[0][0:3] == "Chr":
-                CHR = chr_name[0].replace("Chr", "")
-            # In case the chromosomes do not have prefix but are simple integer
-            elif int(chr_name[0][0]): 
-                CHR = chr_name[0]
+            CHR = line[1].split(":")[0] # Get the chromosome ID
+            # Note that the CHR field should be numeric for plotting R 
+            # manhattan package. Therefore, prefix "Chr" should be removed and organelles
+            # with string names should be skipped
+            
+            # Nothing to do if the chromosome is already a digit
+            if is_digit(CHR):
+                CHR = CHR            
+            # Remove "Chr" prefix if present and check if suffix is a digit
+            elif CHR[0:3] == "Chr" and is_digit(CHR[3::]):
+                CHR = CHR.replace("Chr","")
+            # Skip the line if CHR is something else than e.g. 
+            # a digit or "Chr" + digit
+            else:
+               continue
+        
+            # Get the information for the other fields
             BP = line[2]
             P = line[8]
             zscore = line[7]
